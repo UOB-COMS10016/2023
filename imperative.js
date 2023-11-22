64bf8c7b3b1c68e9fc7152ca348a9e556bc79d66
TiloBurghardt <tb2935@bristol.ac.uk>
2023-11-22 13:34:01 +0000

    Update imperative.js

--- imperative.js
+++ imperative.js
@@ -1,5 +1,5 @@
 //LAYOUT CUSTOMIZATION
-var currentWeek   = 9;                         // current week [releases content fully visible up to this week]
+var currentWeek   = 10;                         // current week [releases content fully visible up to this week]
 var activityNum   = 8;                         // number of activities per week (empty slots possible)
 var columnNum     = 4;                         // desired columns per week (yet, autofitted to max 2 rows per week)
 var title         = "IMPERATIVE PROGRAMMING";  // content title (different to unitName since multiple content streams maybe in one unit)
